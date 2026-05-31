12bb0611781252380e8ab347bdc2e90a5dba3503
Fernando Gabriel Feitosa Leite <fernando.leite@nees.ufal.br>
2026-05-31 00:16:03 -0300

    refactor: reorder import statements for better organization

--- src/api/router.py
+++ src/api/router.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
-from src.api.letters import router as letters_router
-from src.api.guesses import router as guesses_router
-from src.api.auth import router as auth_router
 from src.api.admin import router as admin_router
+from src.api.auth import router as auth_router
+from src.api.guesses import router as guesses_router
+from src.api.letters import router as letters_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 api_router.include_router(letters_router, prefix="/letters", tags=["letters"])
 api_router.include_router(guesses_router, prefix="/guesses", tags=["guesses"])
 api_router.include_router(admin_router, prefix="/admin", tags=["admin"])
+
+
+@api_router.get("/", tags=["root"])
+async def root():
+    return {"message": "Welcome to the Carta Anônima API!"}
